c1b927ae6d2ee131ade41c3bfe76b0bc1b6912b4
freemax-0602 <nicitka139@gmail.com>
2026-09-24 16:46:07 +0300

    feat(http): add validation and CTest coverage

--- workers/http/HttpWorker.cpp
+++ workers/http/HttpWorker.cpp
@@ -21,3 +21,42 @@ std::string HttpWorker::description() const
 {
     return "http GET for current URL";
 }
+
+bool HttpWorker::validate(const HttpArguments& args, std::string& error) const
+{
+    if (args.url.empty())
+    {
+        error = "URL is required";
+        return false;
+    }
+
+    if (!args.url.starts_with("http://") &&
+        !args.url.starts_with("https://"))
+    {
+        error = "URL must use http or https scheme";
+        return false;
+    }
+}
+
+HttpResult HttpWorker::execute(const HttpArguments& args) const
+{
+    std::string error;
+
+    if (!validate(args, error))
+    {
+        return
+        {
+            false,
+            0,
+            "",
+            error
+        };
+    }
+    return
+    {
+        true,
+        200,
+        "HTTP Get is not implemented yet",
+        ""
+    };
+}
