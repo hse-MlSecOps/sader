777dc90c5886dd336e058a312c41f4e8c4bb27de
Sergei Gousev <ssgusev@edu.hse.ru>
2026-09-24 17:36:56 +0300

    fix: адаптация TextWorker к общим типам Schema/Result/Arguments из Worker.h

--- workers/text/TextWorker.h
+++ workers/text/TextWorker.h
@@ -9,30 +9,12 @@
 class TextWorker : public Worker
 {
 public:
-    struct Argument
-    {
-        std::string name;
-        std::string type;
-        bool required;
-        std::string limits;
-    };
-
-    struct Result
-    {
-        bool ok;
-        std::string value;
-        std::string error;
-    };
-
-    using Schema = std::vector<Argument>;
-    using Arguments = std::map<std::string, std::string>;
-
     TextWorker();
     ~TextWorker() override;
 
     std::string name() const override;
     std::string description() const override;
 
-    Schema schema() const;
-    Result execute(const Arguments& args) const;
+    Schema schema() const override;
+    Result execute(const Arguments& args) override;
 };
